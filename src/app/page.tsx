5769f30ddbfa971b1907171a34812a8aaed9e30f
Rohit kumar yadav <rohitkuyada@gmail.com>
2025-10-06 17:49:35 +0530

    feat: initialize Next.js application with Tailwind CSS setup
    - Added PostCSS configuration for Tailwind CSS.
    - Created global CSS file with Tailwind CSS imports and custom styles.
    - Implemented RootLayout component with Google Fonts integration.
    - Developed Homepage component with Tailwind CSS styling.
    - Configured TypeScript settings for the project.

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold mb-4">Welcome to the Homepage</h1>
+      <p className="text-lg text-gray-700">This is a sample Next.js application with Tailwind CSS.</p>
+      
+    </div>
+  )
+}
+
+export default Homepage
