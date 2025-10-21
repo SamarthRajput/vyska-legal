2cdd453d15fcde6eb84deb6ef03b3f26860eeed6
Samarth Rajput <ssamarth224@gmail.com>
2025-10-21 18:59:14 +0530

    user dashboard updated

--- src/app/(user-dashboard)/layout.tsx
+++ src/app/(user-dashboard)/layout.tsx
@@ -1,19 +1,10 @@
-import { getCurrentUser } from "@/actions/syncUser";
 import DashboardLayout from "@/components/dashboard/layout/DashboardLayout";
-import ShowError from "@/components/ShowError";
 
 export default async function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-
-    const user = await getCurrentUser();
-
-    // Check if user exists and is user 
-    if (!user || user.role !== "USER") {
-        return <ShowError message="Access Denied. You must be signed in to view this page" ></ShowError>
-    }
 
     return (
         <div>
